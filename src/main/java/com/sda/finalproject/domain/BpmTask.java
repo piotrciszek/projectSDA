0af4ae232ad93cb5ad6e20c8ff839889bf716d2a
Piotr Ciszek <piotr_ciszek@poczta.onet.pl>
2018-07-31 20:31:59 +0200

    add bpmTask domain

---
+++ src/main/java/com/sda/finalproject/domain/BpmTask.java
@@ -0,0 +1,80 @@
+package com.sda.finalproject.domain;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class BpmTask {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String title;
+    private String description;
+    private String comment;
+    private LocalDateTime addTime;
+    private boolean done;
+
+    public BpmTask(String title, String description, LocalDateTime addTime) {
+        this.title = title;
+        this.description = description;
+        this.addTime = addTime;
+    }
+
+    public BpmTask() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public LocalDateTime getAddTime() {
+        return addTime;
+    }
+
+    public void setAddTime(LocalDateTime addTime) {
+        this.addTime = addTime;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
+
