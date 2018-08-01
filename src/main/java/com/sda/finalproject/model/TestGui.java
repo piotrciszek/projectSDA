fdfa1f523e57d5a8b970bbe3a0678c6980a110a5
Maciej <zaambarasowany@gmail.com>
2018-08-01 17:28:04 +0200

    test2

--- src/main/java/com/sda/finalproject/model/TestGui.java
+++ src/main/java/com/sda/finalproject/model/TestGui.java
@@ -3,11 +3,12 @@ package com.sda.finalproject.model;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 
-@SpringUI(path = "/test")
+@SpringUI(path = "/test2222")
 public class TestGui extends UI{
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-
+        VerticalLayout verticalLayout = new VerticalLayout();
     }
 }
