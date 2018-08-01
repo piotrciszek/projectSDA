091316c962d97898babaddd38dcba2f8361303bc
Piotr Ciszek <piotr_ciszek@poczta.onet.pl>
2018-08-01 17:26:52 +0200

    testGui, test git

---
+++ src/main/java/com/sda/finalproject/model/TestGui.java
@@ -0,0 +1,13 @@
+package com.sda.finalproject.model;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.UI;
+
+@SpringUI(path = "/test")
+public class TestGui extends UI{
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+
+    }
+}
